7bdd4a04ea16cf6a7c0de38456655e1bb7df15ac
tom lee <tdroid99@gmail.com>
2018-08-07 13:16:11 -0600

    added queries

--- queries.js
+++ queries.js
@@ -2,13 +2,20 @@ const database = require("./database-connection");
 
 module.exports = {
     list(){
+        return database('reso-server').select();
     },
     read(id){
+        return database('reso-server').select().where('id', id);
     },
     create(resolution){
+        return database('reso-server').insert(resolution).returning('*')
+            .then(returnedArray => returnedArray[0]);
     },
     update(id, resolution){
+        return database('reso-server').update(resolution).where('id', id)
+            .returning('*').then(returnedArray => returnedArray[0]);
     },
     delete(id){
+        return database('reso-server').delete().where('id', id);
     }
 };
